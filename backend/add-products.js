9e960b3e7cbb48891d3d4c78257289071e2e8ab5
VektorTech <kennymax18@gmail.com>
2020-01-14 20:36:20 -0500

    added products to db

---
+++ backend/add-products.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.DB_URI_STRING, {useNewUrlParser: true});
+
+const products = mongoose.model('products', {
+    name: String ,
+    category: String,
+    description: String,
+    price: String,
+    imgLow:String,
+    imgHigh: String   
+});
+
+const prods = require('./products.json');
+prods.map( (elem) => { 
+    const prod = new products(elem);
+    prod.save();
+});
